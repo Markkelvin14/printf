f8490295f1254b675ea9c5d4d4aa69fd8b190de2
Mustyatiq <113512997+Mustyatiq@users.noreply.github.com>
2023-04-01 00:56:36 +0200

    Update _char.c

--- _char.c
+++ _char.c
@@ -4,7 +4,7 @@
  * _char -  writes character to main buffer from _printf()
  * @buffer: main buffer(from printf())
  * @count: index for main buffer(from _printf())
- * @char: argument from va_arg from _printf()
+ * @n: argument from va_arg from _printf()
  */
 
 void _char(void *buffer, unsigned int *count, int n)
