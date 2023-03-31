a2187b25d41005c8fd5ffaa6c5e4a6364ee8098e
Mustyatiq <113512997+Mustyatiq@users.noreply.github.com>
2023-04-01 01:25:05 +0200

    Update main.h

--- main.h
+++ main.h
@@ -10,6 +10,7 @@ int _size(char *str);
 int _formatsize(char * str);
 char* _tochar(int);
 void _search_specifier(void *buffer,unsigned int *count, va_list* arg, char c);
+void _char(void *buffer, unsigned int *count, int);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void _string(void *buffer, unsigned int *count, char* str);
 void _integer(void * buffer, unsigned int* count, int n);
